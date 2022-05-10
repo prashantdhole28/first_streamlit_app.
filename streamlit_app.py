af2b4bbe5292ee2bc45639d5e8c4cafb74e418ab
prashantdhole28 <105294823+prashantdhole28@users.noreply.github.com>
2022-05-10 21:50:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.text(fruityvice_response)
 
 streamlit.header('Fruityvice Fruit Advice!')
 
-import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
