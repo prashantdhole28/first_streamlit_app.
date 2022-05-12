aaf90d92880a9c17f60d7c984c838a0a8ae996ac
prashantdhole28 <105294823+prashantdhole28@users.noreply.github.com>
2022-05-12 21:15:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,5 +91,7 @@ if streamlit.button('Get Fruit List'):
    my_data_rows = get_fruit_load_list()
    my_cnx.close()
    streamlit.dataframe(my_data_rows)
+   
+   
                                      
 
